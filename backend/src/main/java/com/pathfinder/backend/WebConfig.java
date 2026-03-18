3167b61587afc94487661b80b15d50ed9270f3f0
Tarash Pavlo <pavlo.tarash@etu.unistra.fr>
2026-03-19 00:23:00 +0100

    fix CORS

--- backend/src/main/java/com/pathfinder/backend/WebConfig.java
+++ backend/src/main/java/com/pathfinder/backend/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*");
     }
